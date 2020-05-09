7bb04b95b6c0ac8b2f40ebbb4d22b2ce65c36da4
Phillip <philliphlapa2015@gmail.com>
2020-05-09 22:17:47 +0200

    comment line

--- src/main/java/com/org/Test.java
+++ src/main/java/com/org/Test.java
@@ -26,6 +26,7 @@ public class Test extends HttpServlet {
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		// TODO Auto-generated method stub
 		//i need to do something about this eclipse, it is so slow....
+		//ill fix my eclipse tomorrow
 		response.getWriter().append("Served at: ").append(request.getContextPath());
 	}
 
